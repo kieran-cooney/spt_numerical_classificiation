0c00d23b5c60ead9e16f12a233aa6a846136dd57
Kieran Cooney <kieran.cooney@hotmail.com>
2024-07-05 15:47:48 -0400

    Added hessian functions to gradients.py

--- code/SPTOptimization/gradients.py
+++ code/SPTOptimization/gradients.py
@@ -12,8 +12,10 @@ To-do:
 * Initially thought I needed the gradients_outside_right_boundary_unitaries
   and corresponding left function, but found a way around. Testing shows there
   is likely an error with the left function.
-* Be a bit more uniform with the language in general. Matrix vs expectation
-  etc.
+* Be a bit more uniform with the language in general. Matrix vs expectation,
+  operators instead of unitaries, etc.
+* There are some formulae here without much explanation, should refer to
+  derivations somewhere.
 """
 
 from functools import reduce
@@ -388,7 +390,6 @@ def expectation_gradients(psi, transfer_matrices, left_site_index,
     return gradients
 
 
-
 def gradients_outside_left_boundary_unitaries(psi, right_environment,
     num_outside_sites, left_site_index):
     """
@@ -494,3 +495,284 @@ def gradients_outside_right_boundary_unitaries(psi, left_environment,
 
     return gradients
     
+
+def get_expectation_hessian(psi, left_site_index, unitaries, transfer_matrices,
+    left_environment=None, right_environment=None, mps_form='B'):
+    """
+    Given an mps psi and a sequence of operators  acting on the wavefunction
+    (of which unitaries need only form a subset), calculate the hessian of the
+    expectation of those operators with respect to variations in each of the
+    unitaries. Specific variations are chose to preserve unitarity and
+    redundant variations (changes by an overall phase) are ignored.
+
+    Parameters
+    ----------
+    psi: tenpy.networks.mps.MPS
+        The MPS being operated on
+    left_site_index: integer
+        Gradients will be computed for sites to the left of this site index
+        (inclusive).
+    unitaries: list of square numpy arrays
+        List of operators acting on a consecutive sequence of sites of psi,
+        beginning at left_site_index and proceeding to the right.
+    transfer_matrices: list of tenpy.linalg.np_conserved.Array
+        List of transfer matrices for each unitary of unitaries represented as
+        tenpy arrays with 4 legs.
+    left_environment: tenpy.linalg.np_conserved.Array with legs vR, vR* 
+        Represents the expected value contribution from those sites to the
+        left of the left_site_index (exclusive). If not specified or set to
+        'None', then the identity environment is used.
+    right_environment: tenpy.linalg.np_conserved.Array with legs vL, vL* 
+        Represents the expected value contribution from those sites to the
+        right of unitaries (exclusive). If not specified or set to 'None',
+        then the identity environment is used.
+    mps_form: string
+        The "gauge" to use when calculating the matrix gradient. Passed to
+        MPS.get_B from the tenpy package. This should be chosen based on the
+        gauge used for the left and right environments.
+
+    Returns
+    -------
+    symmetric square numpy array of shape (n, 3, n, 3) where n=len(unitaries)
+        Hessian matrix. The [i,j,k,l] element of the array corresponds to
+        varying the i-th unitary in the j-th direction and the k-th unitary
+        in the l-th direction.
+
+    To-do:
+        * Generalize to arbitary site dimensions
+    """
+    n_unitaries = len(unitaries)
+
+    hessian_array = np.zeros((n_unitaries, 3, n_unitaries, 3), dtype=complex)
+
+    perturbations = np.array([np.dot(non_trivial_hermitians, u) for u in unitaries])
+
+    if left_environment is None:
+        left_environment = get_left_identity_environment(psi, left_site_index)
+
+    if right_environment is None:
+        right_site_index = left_site_index + n_unitaries - 1
+        right_environment = get_right_identity_environment(
+            psi, right_site_index
+        )
+
+    # Recursively update hessian_array
+    recursive_expectation_hessian(
+        psi,
+        left_site_index,
+        left_environment,
+        right_environment,
+        unitaries,
+        perturbations,
+        transfer_matrices,
+        hessian_array,
+        mps_form
+    )
+
+    return hessian_array
+
+def recursive_expectation_hessian(psi, left_site_index, left_environment,
+    right_environment, unitaries, perturbations, transfer_matrices,
+    hessian_array, mps_form):
+    """
+    Given an mps psi and a sequence of operators  acting on the wavefunction
+    (of which unitaries need only form a subset), calculate the hessian of the
+    expectation of those operators with respect to variations in the first of
+    elemnt of unitaries and one other from the rest, then this function is
+    called again on the rest of the unitaries to caluclate the full hessian.
+
+    When called, modifies hessian_array in place and returns 'None'.
+
+    Parameters
+    ----------
+    psi: tenpy.networks.mps.MPS
+        The MPS being operated on
+    left_site_index: integer
+        Gradients will be computed for sites to the left of this site index
+        (inclusive).
+    left_environment: tenpy.linalg.np_conserved.Array with legs vR, vR* 
+        Represents the expected value contribution from those sites to the
+        left of the left_site_index (exclusive). If not specified or set to
+        'None', then the identity environment is used.
+    right_environment: tenpy.linalg.np_conserved.Array with legs vL, vL* 
+        Represents the expected value contribution from those sites to the
+        right of unitaries (exclusive). If not specified or set to 'None',
+        then the identity environment is used.
+    unitaries: list of square numpy arrays
+        List of operators acting on a consecutive sequence of sites of psi,
+        beginning at left_site_index and proceeding to the right.
+    perturbations: numpy array of shape (n, 3, 2, 2) where n=len(unitaries)
+        Basis of perturbations for each of unitary of unitaries. For index
+        [i,j,k,l], i indexes the unitary/site, j indexes the perturbation
+        direction, k and l index the input and output dimension of the 2x2
+        matrix perturbation.
+    transfer_matrices: list of tenpy.linalg.np_conserved.Array
+        List of transfer matrices for each unitary of unitaries represented as
+        tenpy arrays with 4 legs.
+    hessian_array: numpy array of shape (n, 3, n, 3)
+        See output of get_expectation_hessian for explanation.
+    mps_form: string
+        The "gauge" to use when calculating the matrix gradient. Passed to
+        MPS.get_B from the tenpy package. This should be chosen based on the
+        gauge used for the left and right environments.
+
+    Returns
+    -------
+    None
+
+    To-do:
+        * Generalize to arbitary site dimensions
+        * When calculating tail gradients for multiple recursion calls,
+          repeating transfer matrix multiplcations. Could cache somehow for a
+          speed up.
+    """
+    # Split lists into head and tail, a la function recursion.
+    head_unitary, *tail_unitaries = unitaries
+    head_perturbations, *tail_perturbations = perturbations
+    head_tm, *tail_tms = transfer_matrices
+
+    # For each of the perturbations on the first unitary, calculate the
+    # resulting left environment immediately to the right of left_site_index.
+    perturbation_tms = [
+        get_transfer_matrix_from_unitary(
+            psi, left_site_index, u, form=mps_form
+        )
+        for u in head_perturbations
+    ]
+
+    new_left_environments = [
+        multiply_transfer_matrices(left_environment, tm)
+        for tm in perturbation_tms
+    ]
+
+    # For each of the proceeding left environments, calculate the expectation
+    # gradients for the rest of the sites.
+    tail_gradients = [
+        expectation_gradients(
+            psi, tail_tms, left_site_index+1, le, right_environment, mps_form
+        )
+        for le in new_left_environments
+    ]
+
+    # Ugly for loops... i, j, k index the head perturbation direction, tail
+    # unitary and tail perturbation respectively.
+    for i, gradients in enumerate(tail_gradients):
+        triples = enumerate(zip(gradients, tail_perturbations), start=1)
+        for j, (gradient, perturbations) in triples:
+            for k, perturbation in enumerate(perturbations):
+                np_grad = gradient.to_ndarray()
+                out = np.sum(np_grad*perturbation)
+                # hessian symmetric so update two entries
+                hessian_array[0, i, j, k] = out
+                hessian_array[j, k, 0, i] = out
+
+    # left environment for next iteration
+    new_left_environment = multiply_transfer_matrices(
+        left_environment, head_tm
+    )
+
+    # If two or more unitaries left, repeat the above by moving the left site
+    # one unit to the right and only considering sites to the right
+    # (inclusive) of the new left site.
+    if len(tail_unitaries) > 1:
+        recursive_expectation_hessian(
+            psi,
+            left_site_index+1,
+            new_left_environment,
+            right_environment,
+            tail_unitaries,
+            tail_perturbations,
+            tail_tms,
+            hessian_array[1:, :, 1:, :],
+            mps_form
+        )
+
+
+def get_expectation_gradient_fixed_basis(expectation_gradients, unitaries):
+    """
+    For an (unspecified) mps with operators, of which unitaries form a subset,
+    and associated gradients of the expectation expectation_gradients,
+    calculate gradients with respect to a fixed basis of perturbations on each
+    unitary of unitaries.
+
+    Parameters
+    ----------
+    expectation_gradients: list of square tenpy arrays with legs ['p*', 'p']
+        List of gradients of the expectation with respect to the sequence
+        unitaries.
+    unitaries: list of square numpy arrays
+        List of operators acting on a consecutive sequence of sites of an mps.
+
+    Returns
+    -------
+    numpy array of shape (n, 3) 
+    where n=len(unitaries)=len(expectation_gradients)
+        Vector of partial derivatives of the expectation with respect to
+        perturbations in each of the unitary. The [i,j] element corresponds to
+        varying the i-th unitary in the j-th direction.
+
+    To-do:
+        * Perhaps 'gradient' is the wrong term here...
+    """
+    # Convert gradients from tenpy array to numpy array
+    gradients = np.array([g.to_ndarray() for g in expectation_gradients])
+    # Calculate perturbations to unitaries
+    perturbations = np.array([np.dot(non_trivial_hermitians, u) for u in unitaries])
+
+    # Calculate the resulting change in expectation by a simple contraction on
+    # the appropriate index.
+    perturbation_gradients = np.sum(
+        gradients[:, np.newaxis, ...] * perturbations,
+        (-1, -2)
+    )
+
+    return perturbation_gradients
+
+def get_quadratic_expectation_hessian(expectation, expectation_gradients,
+                                      expectation_hessian):
+    """
+    Given an (unspecified) mps operated on by some (unspecified) unitaries
+    with associated expectation, and the gradients and hessian of the
+    expectation with respect to perturbations in some of those unitaries,
+    calculate and return the hessian for the norm squared of the expectation
+    with respect to those same perturbations.
+
+    Parameters
+    ----------
+    expectation: The expectation of the mps with respect to some unitaries.
+    expectation_gradients: numpy array of shape (N, 3)
+        List of gradients of the expectation with respect to some subset of
+        the unitaries used to calculate the expectation. See output of
+        get_expectation_gradient_fixed_basis.
+    expectation_hessian: symmetric square numpy array of shape (N, 3, N, 3)
+        The [i,j,k,l] element of the array corresponds to varying the i-th
+        unitary in the j-th direction and the k-th unitary in the l-th
+        direction. See output of get_expectation_hessian.
+
+    Returns
+    -------
+    symmetric numpy array of shape (3*n, 3*n)
+        Hessian matrix for |expectation|**2 with respect to fixed
+        perturbations of unitaries as specified by the input gradients and
+        hessian.
+    """
+    # If N is the number of sites/unitaries the hessian is calculated for,
+    # then n=3*N is the total dimension of the tangent space in consideration.
+    n = expectation_hessian.shape[0]*expectation_hessian.shape[1]
+
+    # Collapse the aces of the expectation_hessian and compute contribution to
+    # the quadratic expectation hessian.
+    flat_exp_hessian = np.reshape(expectation_hessian, (n,n))
+    h1 = np.real(expectation*np.conj(flat_exp_hessian))
+
+    # Flatten the expectation gradients.
+    flat_exp_grads = np.reshape(expectation_gradients, (n))
+
+    # Calculate the contribution to the quadratic expectation hessian.
+    second_order_from_first_hessian = (
+        flat_exp_grads[:, np.newaxis] + flat_exp_grads[np.newaxis, :]
+    )
+
+    h2 = np.abs(second_order_from_first_hessian)**2
+
+    return 2*(h1 + h2)
