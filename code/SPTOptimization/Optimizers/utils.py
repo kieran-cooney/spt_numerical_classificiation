31cc01aa17fded74f11bc881608e4de7199cd5c1
Kieran Cooney <kierancooney@Kierans-MacBook-Air-5.local>
2024-08-13 14:32:22 -0400

    Added numerous optimization util functions

--- code/SPTOptimization/Optimizers/utils.py
+++ code/SPTOptimization/Optimizers/utils.py
@@ -1,22 +1,65 @@
 from itertools import accumulate
 
+import numpy as np
 import tenpy.linalg.np_conserved as npc
 
 from SPTOptimization.utils import (
-    get_transfer_matrix_from_tp_unitary,
-    get_transfer_matrices_from_tp_unitary_list,
+    get_transfer_matrix_from_tp_unitary_and_b_tensor,
     multiply_transfer_matrices,
     multiply_transfer_matrices_from_right,
-    get_right_identity_environment,
-    get_left_identity_environment
+    get_right_identity_environment_from_tp_tensor,
+    get_left_identity_environment_from_tp_tensor
 )
 
 from SPTOptimization.gradients import (
-    expectation_gradient_from_environments
+    expectation_gradient_from_environments_and_b_tensor
 )
 
 
-def right_one_site_optimization(left_environment, psi, unitaries):
+def max_expectation(grad):
+    """
+    Given a tensor grad which represents an contraction of tensors yielding an
+    expectation value except for one unitary which has not been contracted,
+    return the largest possible value for the expectation over all such
+    unitaries.
+
+    Parameters
+    ----------
+    grad: tenpy.linalg.np_conserved.Array with legs 'p' and 'p*'
+        A tensor which if contracted against an operator yields an expectation.
+    Returns
+    -------
+    float
+        The best possible expectation over all unitaries.
+    """
+    S = npc.svd(grad.complex_conj(), compute_uv=False)
+    return np.sum(S)
+
+
+def one_site_optimization(grad):
+    """
+    Given a tensor grad which represents an contraction of tensors yielding an
+    expectation value except for one unitary which has not been contracted,
+    return the largest possible value for the expectation over all such
+    unitaries and the unitary which achieves this maximum.
+
+    Parameters
+    ----------
+    grad: tenpy.linalg.np_conserved.Array with legs 'p' and 'p*'
+        A tensor which if contracted against an operator yields an expectation.
+    Returns
+    -------
+    pair of tenpy.linalg.np_conserved.Array with legs 'p' and 'p*' and float
+        The best possible expectation over all unitaries and the unitary
+        attaining this maximum.
+    """
+    U, S, VH = npc.svd(grad.complex_conj(), compute_uv=False)
+    out_unitary = npc.tensordot(U, VH, [['i',], ['i*',]])
+    return (np.sum(S), out_unitary)
+
+
+def one_site_optimization_sweep_right(left_environment, mps_tensors,
+    unitaries, mps_tensors_bra=None, right_environments=None):
     """
     Given an MPS psi with environment immediately to the left of it, and a
     set of one site unitaries, optimize each of the unitaries in turn and
@@ -26,67 +69,93 @@ def right_one_site_optimization(left_environment, psi, unitaries):
     ----------
     left_environment: tenpy.linalg.np_conserved.Array
         Evnironment to the left of psi. Tensor with legs 'vR' and 'vR*'.
-    psi: tenpy.networks.mps.MPS
-        The MPS the unitaries are optimizing against.
+    mps_tensors: list of tenpy.linalg.np_conserved.Array
+        The i-th element should correspond to the "B" tensor (or some other
+        gauge) of some MPS psi. Assumed the tensors belong to adjacent sites
+        in psi.
     unitaries: List of tenpy.npc.Array
-        List of single site operators. Should be same length as the number of
-        sites in psi.
+        List of single site operators. Should be same length mps_tensors.
+    mps_tensors_bra: list of tenpy.linalg.np_conserved.Array
+        If provided, then will be used as tensors for the "bra" in the
+        expectation value expression. If not provided, then mps_tensors are
+        used. 
+    right_environments: list of tenpy.linalg.np_conserved.Array
+        A list of 2-leg tensors with legs 'vL', 'vL*'. The i-th element
+        corresponds to the contribution to the expectation immediately to the
+        right of site i, prior to any unitaries being updated.
+        If not provided, will be calculated.
     Returns
     -------
-    pair of (list of float, tenpy.linalg.np_conserved.Array)
-        For the first element in the pair, the i-th element corresponds to
+    triple of (list of float, tenpy.linalg.np_conserved.Array, list of
+    tenpy.linalg.np_conserved.Array)
+        For the first element in the triple, the i-th element corresponds to
         the expectation after optimizing the unitary at the i-th site.
         For the second, last left environment found with legs 'vR' and 'vR*'.
+        The third is a list of the calculated transfer matrices.
     """
-    num_sites = psi.L
+    num_sites = len(mps_tensors)
 
-    last_right_environment = get_right_identity_environment(psi, num_sites)
-
-    transfer_matrices = get_transfer_matrices_from_tp_unitary_list(
-        psi, 0, unitaries, form='B'
+    last_right_environment = get_right_identity_environment_from_tp_tensor(
+        mps_tensors[-1]
     )
 
-    right_environments = list(accumulate(
-        transfer_matrices[::-1],
-        multiply_transfer_matrices_from_right,
-        initial=last_right_environment
-    ))[::-1]
+    if mps_tensors_bra is None:
+        mps_tensors_bra = mps_tensors
+
+    if right_environments is None:
+        transfer_matrices = [
+            get_transfer_matrix_from_tp_unitary_and_b_tensor(b1, u, b2)
+            for b, u, b2 in zip(mps_tensors, unitaries, mps_tensors_bra)
+        ]
+
+        # Slice so we avoid calculating the inner most right environment, which
+        # is unused.
+        right_environments = list(accumulate(
+            transfer_matrices[:1:-1], 
+            multiply_transfer_matrices_from_right,
+            initial=last_right_environment
+        ))[::-1]
 
     current_left_environment = left_environment
     expectations = list()
 
-    for i in range(num_sites):
-    # Compute gradient
-    grad = expectation_gradient_from_environments(
-        psi,
-        i,
-        left_environment=current_left_environment,
-        right_environment=right_environments[i],
-        mps_form='B'
+    triples = zip(
+        mps_tensors,
+        right_environments,
+        mps_tensors_bra
     )
 
-    # Optimize unitary, update in place
-    U, S, VH = npc.svd(grad.complex_conj(), inner_labels = ['i', 'i*'])
+    new_transfer_matrices = list()
 
-    new_unitary = npc.tensordot(U, VH, [['i',], ['i*',]])
-    unitaries[i] = new_unitary
-    transfer_matrix = get_transfer_matrix_from_tp_unitary(
-        psi, i, new_unitary, form='B'
-    )
+    for b, right_environment, b_bra in triples:
+        # Compute gradient
+        grad = expectation_gradient_from_environments_and_b_tensor(
+            b, current_left_environment, right_environment, b_bra
+        )
 
-    # Save new expectation.
-    expectations[i] = np.sum(S)
+        # Optimize unitary, update in place
+        new_expectation, new_unitary = one_site_optimization(grad)
 
-    # Create new left environment
-    current_left_environment = multiply_transfer_matrices(
-        transfer_matrix,
-        current_left_environment
-    )
+        unitaries[i] = new_unitary
+        expectations[i] = new_expectation
 
-    return (expectations, current_left_environment)
+        # Create new left environment
+        transfer_matrix = get_transfer_matrix_from_tp_unitary_and_b_tensor(
+            b, new_unitary, b_bra
+        )
+
+        new_transfer_matrices.append(transfer_matrix)
+
+        current_left_environment = multiply_transfer_matrices(
+            current_left_environment,
+            transfer_matrix
+        )
+
+    return (expectations, current_left_environment, new_transfer_matrices)
 
 
-def left_one_site_optimization(right_environment, psi, unitaries):
+def one_site_optimization_sweep_left(right_environment, mps_tensors,
+    unitaries, mps_tensors_bra=None, left_environments=None):
     """
     Given an MPS psi with environment immediately to the right of it, and a
     set of one site unitaries, optimize each of the unitaries in turn and
@@ -95,31 +164,86 @@ def left_one_site_optimization(right_environment, psi, unitaries):
     Parameters
     ----------
     right_environment: tenpy.linalg.np_conserved.Array
-        Evnironment to the right of psi. Tensor with legs 'vL' and 'vL*'.
-    psi: tenpy.networks.mps.MPS
-        The MPS the unitaries are optimizing against.
+        Evnironment to the left of psi. Tensor with legs 'vL' and 'vL*'.
+    mps_tensors: list of tenpy.linalg.np_conserved.Array
+        The i-th element should correspond to the "B" tensor (or some other
+        gauge) of some MPS psi. Assumed the tensors belong to adjacent sites
+        in psi.
     unitaries: List of tenpy.npc.Array
-        List of single site operators. Should be same length as the number of
-        sites in psi.
+        List of single site operators. Should be same length mps_tensors.
+    mps_tensors_bra: list of tenpy.linalg.np_conserved.Array
+        If provided, then will be used as tensors for the "bra" in the
+        expectation value expression. If not provided, then mps_tensors are
+        used. 
+    left_environments: list of tenpy.linalg.np_conserved.Array
+        A list of 2-leg tensors with legs 'vR', 'vR*'. The i-th element
+        corresponds to the contribution to the expectation immediately to the
+        left of site i, prior to any unitaries being updated.
+        If not provided, will be calculated.
     Returns
     -------
-    pair of (list of float, tenpy.linalg.np_conserved.Array)
-        For the first element in the pair, the i-th element corresponds to
+    triple of (list of float, tenpy.linalg.np_conserved.Array, list of
+    tenpy.linalg.np_conserved.Array)
+        For the first element in the triple, the i-th element corresponds to
         the expectation after optimizing the unitary at the i-th site.
-        For the second, last right environment found with legs 'vL' and 'vL*'.
+        For the second, last left environment found with legs 'vR' and 'vR*'.
+        The third is a list of the calculated transfer matrices.
     """
-    psi_inv = psi.copy().spatial_inversion()
+    num_sites = len(mps_tensors)
 
-    left_environment = right_environmnet.replace_labels(
-        ['vL', 'vL*'], ['vR', 'vR*']
+    last_left_environment = get_left_identity_environment_from_tp_tensor(
+        mps_tensor
     )
 
-    expectations, last_left_environment = right_one_site_optimization(
-        left_environment, psi, unitaries
+    if mps_tensors_bra is None:
+        mps_tensors_bra = mps_tensors
+
+    if left_environments is None:
+        transfer_matrices = [
+            get_transfer_matrix_from_tp_unitary_and_b_tensor(b1, u, b2)
+            for b, u, b2 in zip(mps_tensors, unitaries, mps_tensors_bra)
+        ]
+
+        # Slice so we avoid calculating the inner most right environment,
+        # which is unused.
+        left_environments = list(accumulate(
+            transfer_matrices[:1:-1], 
+            multiply_transfer_matrices_from_left,
+            initial=last_left_environment
+        ))[::-1]
+
+    current_right_environment = right_environment
+    expectations = list()
+
+    triples = zip(
+        mps_tensors,
+        left_environments,
+        mps_tensors_bra
     )
 
-    last_right_environment = last_left_environment.replace_labels(
-        ['vR', 'vR*'], ['vL', 'vL*']
-    )
+    new_transfer_matrices = list()
+    for b, left_environment, b_bra in triples:
+        # Compute gradient
+        grad = expectation_gradient_from_environments_and_b_tensor(
+            b, left_environment, current_right_environment, b_bra
+        )
 
-    return (expectations, last_left_environment)
+        # Optimize unitary, update in place
+        new_expectation, new_unitary = one_site_optimization(grad)
+
+        unitaries[i] = new_unitary
+        expectations[i] = new_expectation
+
+        # Create new left environment
+        transfer_matrix = get_transfer_matrix_from_tp_unitary_and_b_tensor(
+            b, new_unitary, b_bra
+        )
+
+        new_transfer_matrices.append(transfer_matrix)
+
+        current_right_environment = multiply_transfer_matrices_from_right(
+            current_right_environment,
+            transfer_matrix
+        )
+
+    return (expectations, current_right_environment, new_transfer_matrices)
